d323fdfd3c09484e7e4f91152449ebc3aa532066
Eduardo <edp2013.ep@gmail.com>
2023-12-15 13:00:18 -0300

    url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const api: AxiosInstance = axios.create({
-  //baseURL: "https://invciclico.adaptable.app/",
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://finance-back.adaptable.app/",
+  //baseURL: "http://localhost:3000/",
 });
 
 export default api;
