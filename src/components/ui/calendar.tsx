9418a5fb5b2d9d05edd931b25201053e65140a5a
Eduardo <edp2013.ep@gmail.com>
2023-12-15 12:53:03 -0300

    erro calendario props

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,8 +55,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
+        // @ts-ignore
         IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        // @ts-ignore
         IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
